47a3206e395bab35063fc1e8bf3b991274d88e75
user883311 <tarik.chardon@gmail.com>
2018-02-04 23:23:28 +0800

    Touched up CSS and started JS.

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
-// All time durations are expressed in minutes. 
+// All time durations are expressed in seconds. 
 
-var defaultDuration = 25, defaultBreakDuration = 5;
+var defaultDuration = 25 * 60, defaultBreakDuration = 5 * 60;
+
+
+function startResume() {
+    /*
+    This function renders elapsing time on the work/break clocks, 
+    whichever is applicable. 
+    */
+    let workDuration = document.getElementById("work-minutes").innerHTML * 60
+        + document.getElementById("work-seconds").innerHTML
+        ;
+    let breakDuration = document.getElementById("break-minutes").innerHTML * 60
+        + document.getElementById("break-seconds").innerHTML
+        ;
+    
+
+}
+
+
+
+// When time is elapsing, + and - buttons are inactive. 
+
+// When work time is elapsing, Start/Resume and Stop/Reset buttons are active on that side. 
+
+// When break time is elapsing, Start/Resume and Stop/Reset buttons are active on that side. 
+
+
 
