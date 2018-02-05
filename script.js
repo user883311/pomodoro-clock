9fa175d68aeac149dbfc6323035704e84a6981dd
user883311 <tarik.chardon@gmail.com>
2018-02-05 23:56:20 +0800

    ELapsing time, mostly JS.

--- script.js
+++ script.js
@@ -1,20 +1,48 @@
 // All time durations are expressed in seconds. 
 
 var defaultDuration = 25 * 60, defaultBreakDuration = 5 * 60;
+var elapsing = false;
 
+var workSecondsID = "work-seconds";
+var workMinutesID = "work-minutes";
+var breakSecondsID = "break-seconds";
+var breakSecondsID = "break-seconds";
 
-function startResume() {
+function start() {
     /*
-    This function renders elapsing time on the work/break clocks, 
+    This function renders elapsing time on the work & break clocks, 
     whichever is applicable. 
     */
-    let workDuration = document.getElementById("work-minutes").innerHTML * 60
-        + document.getElementById("work-seconds").innerHTML
-        ;
-    let breakDuration = document.getElementById("break-minutes").innerHTML * 60
-        + document.getElementById("break-seconds").innerHTML
-        ;
-    
+    setInterval(elapsesWorkTime(workMinutesID, workSecondsID), 1000);
+    function elapsesWorkTime(minutesIDstr, secondsIDstr) {
+        console.log(minutesIDstr, secondsIDstr);
+        let seconds = document.getElementById(secondsIDstr).textContent;
+        let minutes = document.getElementById(minutesIDstr).textContent;
+        console.log(minutes, seconds);
+
+        if (seconds == 0) {
+            if (minutes == 0) {
+                // setInterval(elapsesBreakTime, 1000);
+            }
+            else {
+                seconds = 59;
+                minutes--;
+                document.getElementById(secondsIDstr).textContent = seconds;
+                document.getElementById(minutesIDstr).textContent = minutes;
+            }
+        }
+        else {
+            seconds--;
+            
+            if (seconds < 10) {
+                document.getElementById(secondsIDstr).textContent = "0" + seconds;
+            }
+            else {
+                document.getElementById(secondsIDstr).textContent = seconds;
+            }
+        }
+        console.log(seconds);
+    }
 
 }
 
